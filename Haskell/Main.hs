d7f00969984541697d1c98e3829bfd1338c6aa28
Sébastien Watteau <swatteau@gmail.com>
2016-07-30 11:47:10 +0200

    Initial commit

---
+++ Haskell/Main.hs
@@ -0,0 +1,42 @@
+-- Copyright 2016 Sébastien Watteau
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--     http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+module Main where
+
+import Data.Ratio
+import System.Environment
+
+import Rnrt
+
+closest :: Integer -> Rational -> Rational -> Rational
+closest n q eps =
+    let seq = rnrt n q
+    in head $ dropWhile (\r -> abs (q - r ^ n) > eps ) seq
+
+
+main :: IO ()
+main = do
+  [arg_n, arg_q, arg_eps] <- getArgs
+  let n = read arg_n :: Integer
+  let q = read arg_q :: Rational
+  let eps = read arg_eps :: Rational
+  let result = closest n q eps
+  let decimal = fromRational result
+  putStr "Closest rational number within given tolerance: "
+  print result
+  putStr "Decimal approximation: "
+  print decimal
+  putStr "Control: "
+  print (decimal ^ n)
+
+
---
+++ Haskell/Rnrt.hs
@@ -0,0 +1,63 @@
+-- Copyright 2016 Sébastien Watteau
+-- 
+-- Licensed under the Apache License, Version 2.0 (the "License");
+-- you may not use this file except in compliance with the License.
+-- You may obtain a copy of the License at
+-- 
+--     http://www.apache.org/licenses/LICENSE-2.0
+-- 
+-- Unless required by applicable law or agreed to in writing, software
+-- distributed under the License is distributed on an "AS IS" BASIS,
+-- WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+-- See the License for the specific language governing permissions and
+-- limitations under the License.
+module Rnrt (rnrt) where
+
+import Control.Applicative ((<$>))
+import Control.Monad.State
+import Data.Ratio
+
+mediant :: Rational -> Rational -> Rational
+mediant a b = (numerator a + numerator b) % (denominator a + denominator b)
+
+inverse :: Rational -> Rational
+inverse r = (denominator r) % (numerator r)
+
+type SBNode = (Rational, Rational)
+
+value :: SBNode -> Rational
+value (a, b) = mediant a b
+
+left :: SBNode -> SBNode
+left node@(a, b) = (a, value node)
+
+right :: SBNode -> SBNode
+right node@(a, b) = (value node, b)
+
+rnrt :: Integer -> Rational -> [Rational]
+rnrt n q =
+    let initError = q
+    in if q > 1
+         then map inverse $ evalState (rnrt' n (inverse q)) initError
+         else evalState (rnrt' n q) initError
+
+rnrt' :: Integer -> Rational -> State Rational [Rational]
+rnrt' n q = go (0, 1)
+  where
+    go :: SBNode -> State Rational [Rational]
+    go node = do
+        smallestError <- get
+        let nodeValue = value node
+            candidate = nodeValue ^ n
+            currError = q - candidate
+        if abs currError < abs smallestError
+            then do put currError
+                    (:) nodeValue <$> selectNext node currError
+            else selectNext node currError
+
+    selectNext :: SBNode -> Rational -> State Rational [Rational]
+    selectNext node error
+        | error < 0 = go (left node)
+        | error > 0 = go (right node)
+        | otherwise = return []
+
